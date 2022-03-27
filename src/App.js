f63234dba82e4b10acb77b06123a5f6f25cef2a2
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-27 09:25:52 -0700

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Title from "./components/Title";
+import Modal from "./components/Modal";
+import EventList from "./components/EventList";
+import NewEventForm from "./components/NewEventForm";
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([]);
+
+  const addEvent = (event) => {
+    setEvents((preveEvents) => {
+      return [...preveEvents, event];
+    });
+    setShowModal(false);
+  };
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => id !== event.id);
+    });
+  };
+
+  const subtitle = "All the latest events in Marioland";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Marioland Events" subtitle={subtitle} />
+
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
+
+      {showModal && (
+        <Modal>
+          <NewEventForm addEvent={addEvent} />
+        </Modal>
+      )}
+
+      <div>
+        <button onClick={() => setShowModal(true)}>Add New Event</button>
+      </div>
     </div>
   );
 }
